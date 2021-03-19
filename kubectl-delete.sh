b6f8d28640e9a0a8b3d9def43f60f5fc47d923a1
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-03-19 17:37:28 +0900

    release

--- kubectl-delete.sh
+++ kubectl-delete.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-kubectl delete -f generated/default.yml
+kubectl apply -k ./template/overlays/default/service-broker
+kubectl apply -k ./template/overlays/default/kanban-replicator
+kubectl apply -k ./template/overlays/default/send-anything
+kubectl apply -k ./template/overlays/default/status-kanban
+kubectl apply -k ./template/overlays/default/mongo
+kubectl apply -k ./template/overlays/default/redis
