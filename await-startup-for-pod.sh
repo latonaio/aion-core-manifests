968645566fb8943eb1f7af05b1d5ec3278ae70cd
Yurino Oga <yurino.o@latona.io>
2021-10-15 20:53:40 +0900

    release

--- await-startup-for-pod.sh
+++ await-startup-for-pod.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+echo "Waiting for RabbitMQ startup..."
 
+run_in_rabbitmq_pod() {
+    local pod=$(kubectl get pod | grep -E '^rabbitmq-[0-9a-f]+-[0-9a-z]+ ' | sed -E 's/^([^ ]+) .*$/\1/')
+    kubectl exec "$pod" -- "$@"
+    }
+
+rabbitmqctl() {
+    run_in_rabbitmq_pod rabbitmqctl "$@"
+    }
+
+# RabbitMQ の pod が Running になるまで待機
 while ! kubectl get pod | grep -E '^rabbitmq-[0-9a-f]+-[0-9a-z]+ ' | grep Running > /dev/null; do
     sleep 1
 done
 
-# rabbitmqの起動をrunningになるまで待つ
-pod=$(kubectl get pod | grep -E '^rabbitmq-[0-9a-f]+-[0-9a-z]+ ' | sed -E 's/^([^ ]+) .*$/\1/')
-rabbitmqctl() {
-       	kubectl exec "$pod" -it -- rabbitmqctl "$@"
-        }
+# RabbitMQ のポートが開くまで待機
+while ! run_in_rabbitmq_pod bash -c "nc -w 1 127.0.0.1 5672" 2> /dev/null; do
+    sleep 1
+done
 
-echo "Waiting for rabbitmq startup..."
-while ! rabbitmqctl await_startup > /dev/null 2> /dev/null; do
+# RabbitMQ が利用可能になるまで待機
+while ! rabbitmqctl await_startup 2> /dev/null; do
     sleep 1
 done
